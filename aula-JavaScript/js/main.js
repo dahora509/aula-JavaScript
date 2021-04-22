6c557fd75a4657aace019698a4ffe4262a99c18f
Carlos G. Hora <81786842+dahora509@users.noreply.github.com>
2021-04-21 21:51:21 -0300

    Add files via upload

---
+++ aula-JavaScript/js/main.js
@@ -0,0 +1,120 @@
+
+/*
+let line = gets("11 -2").split(" ");
+let A = parseInt(line[0]);
+let B = parseInt(line[1]);
+let total = A + B;
+console.log("X = " + total);
+*/
+/*
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+    //console.log(document.getElementById("agradecimento"));
+    //alert("Obrigado por clicar");
+}
+
+function redirecionar(){
+    window.open("http://ww1.globallabs.academy/");
+    //window.location.href = "http://ww1.globallabs.academy/";
+}
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mause";
+    elemento.innerHTML = "Obrigado por passar o mause";
+    //alert("trocar texto");
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Passe o mause aqui";
+    elemento.innerHTML = "Passe o mause aqui";
+}
+
+function load(){
+    alert("página carregada");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value)
+}
+*/
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+var validar;
+function validaidade(idade){
+    if (idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade");
+validaidade(idade)
+console.log(validar);
+*/
+
+//alert(soma(5, 10));
+/*
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+*/
+
+/*
+var cout;
+for(count=0; count <= 5; count++){
+    alert(count);
+};
+*/
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+}
+*/
+/*
+var idade = prompt("Qual sua idade?");
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+};
+*/
+
+/*
+var frutas = [{nome:"maça", cor:"vermelha"},{nome:"uva", cor:"roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+/*
+var fruta = {nome:"maça", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+
+//var nome = "Carlos Gonçalves";
+//var n1 = 5;
+//var n2 = 6;
+//var frase = "Japão é o melhor time do mundo";
+//alert (nome + " tem " + idade + " anos ");
+//alert (idade + idade2);
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.toLocaleLowerCase());
+//alert(frase.replace("Japão", "Brasil"));
